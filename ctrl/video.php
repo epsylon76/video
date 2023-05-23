d1459861c9dfda4ef242bc2f764ae58ecb4f3152
epsylon76 <johan.pupin@gmail.com>
2023-05-23 09:47:45 +0200

    diff

--- ctrl/video.php
+++ ctrl/video.php
@@ -1,24 +1,25 @@
 <?php
+$dossier = new dossier();
+
 if(isset($_GET['id']) && $partage->check_partage($_GET['cle'], $_GET['id'])){ //mode utilisateur
   //retrouver le chemin via l'id
   $infos = $partage->get_partage($_GET['id']);
-  $chemin = './data'.$infos['chemin'];
+  $chemin = '/data'.$infos['chemin'];
   $email = $infos['email'];
   $mode = "user";
-
+  $taille_fichier = $dossier->HumanSize(filesize($data.$infos['chemin'])); //diff
 }else{ //mode admin
-
-  $chemin='./data'.$_GET['video'];
-
+  $chemin='/data'.$_GET['video'];
   $mode = "admin";
+  $taille_fichier = $dossier->HumanSize(filesize($data.$_GET['video'])); //diff
 }
 
 $nom_fichier = explode("/", $chemin);
 $nom_fichier = array_reverse($nom_fichier);
 $nom_fichier = $nom_fichier[0];
 
-$dossier = new dossier();
-$taille_fichier = $dossier->HumanSize(filesize($chemin));
+
+
 
 
 include('./vue/video.php');
