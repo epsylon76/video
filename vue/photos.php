eefe254e3658478c7c69d36a8f9447557103de38
epsylon76 <johan.pupin@gmail.com>
2023-09-05 15:42:42 +0200

    zip

--- vue/photos.php
+++ vue/photos.php
@@ -1,4 +1,5 @@
-<?php echo $params['analytics']; //analytics seulement sur la vue client ?>
+<?php echo $params['analytics']; //analytics seulement sur la vue client 
+?>
 
 <div class="container">
 
@@ -9,20 +10,26 @@
   <p><strong>Nombre de photos</strong> : <?php echo $nb; ?> </p>
   <p><strong>Taille</strong> : <?php echo number_format($taille / 1048576, 2); ?> Mo</p>
 
-    <div class="text-center" style="margin-top:50px; margin-bottom:50px;">
-      <a class="btn btn-primary btn-lg" id="dl_button" href="?cle=<?php echo $_GET['cle'] ?>&dl_photos=<?php echo $_GET['id'];?>">
-        <i class="fas fa-download"></i> Télécharger toutes les photos
-      </a>
-    </div>
+  <div class="text-center" style="margin-top:50px; margin-bottom:50px;">
+    <a class="btn btn-primary btn-lg" id="dl_button" href="?cle=<?php echo $_GET['cle'] ?>&dl_photos=<?php echo $_GET['id']; ?>">
+      <i class="fas fa-download"></i> Télécharger toutes les photos
+    </a>
+  </div>
 
-    <?php  if(isset($_GET['id'])){ ?>
+  <?php if (isset($_GET['id'])) { ?>
     <a class="btn btn-success" href="?cle=<?php echo $_GET['cle'] ?>"><i class="fas fa-arrow-left">&nbsp;</i>retour</a>
-    <?php } ?>
+  <?php } ?>
 
 </div> <!--container -->
 
 <style>
-body{
-  background-color:white!important;
-}
+  body {
+    background-color: white !important;
+  }
 </style>
+
+<script>
+  $("#dl_button").click(function(){
+    $('#dl_button').text('')
+  })
+</script>
