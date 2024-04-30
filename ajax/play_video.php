417e5d7c593e83d47bf33e75ab4a2cbd28035077
epsylon76 <johan.pupin@gmail.com>
2024-04-30 10:00:18 +0200

    modifs droits

--- ajax/play_video.php
+++ ajax/play_video.php
@@ -1,9 +1,11 @@
 <?php
 include_once('../mdl/stats.php');
 $data = $_POST;
+$size = filesize('.'.$data['chemin']);
+
 
 $stats = new stats();
-$stats->set_stats($data['action'], date("Y-m-d H:i:s") , $data['chemin']);
+$stats->set_stats($data['action'], date("Y-m-d H:i:s") , $size);
 
 
 ?>
--- includes/js/video_photos.js
+++ includes/js/video_photos.js
@@ -1,21 +1,24 @@
 $("#bouton-dl").click(function() {
-    var email = $(this).data('email');
-    var chemin = $(this).data('chemin');
-    var taille = $(this).data('taille');
-    $.ajax({
-      url: '/ajax/clic_dl.php',
-      type: 'POST',
-      data: {'chemin' : chemin, 'email' : email, 'taille' : taille ,'action' : 'dl_video'}
-    });
+  var email = $(this).data('email');
+  var chemin = $(this).data('chemin');
+  var taille = $(this).data('taille');
+  $.ajax({
+    url: '/ajax/clic_dl.php',
+    type: 'POST',
+    data: {'chemin' : chemin, 'email' : email, 'taille' : taille ,'action' : 'dl_video'}
   });
+});
 
 
 $('#id-video').on('play', function() {
-  var chemin = $(this).data('chemin');
-  console.log('test');
-    $.ajax({
-      url: '/ajax/play_video.php',
-      type: 'POST',
-      data: {'chemin' : chemin ,'action' : 'play_videos'},
-    });
+var chemin = $(this).data('chemin');
+console.log(chemin);
+  $.ajax({
+    url: '/ajax/play_video.php',
+    type: 'POST',
+    data: {'chemin' : chemin ,'action' : 'play_videos'},
+    success: function(data, textStatus, jqXHR){
+        console.log('test2');
+    }
+  });
 });
